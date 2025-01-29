2af62e80101ba44c9c8e4a81b6cb0899a767206e
Leo <ando9836@jec.ac.jp>
2025-01-29 11:32:49 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/cardimagedemo/MainActivity.java
+++ app/src/main/java/com/example/cardimagedemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,15 +32,22 @@ public class MainActivity extends AppCompatActivity {
         EditText edtCardId = findViewById(R.id.edt_card_id);
         Button btnEnter = findViewById(R.id.btn_enter);
         ImageView imgCard = findViewById(R.id.img_card);
+        TextView txtBookInfo = findViewById(R.id.txt_book_info);
 
         // NFCカードIDを入力してボタンを押すと、対応する画像を表示する
         btnEnter.setOnClickListener(v -> {
-            String cardId = edtCardId.getText().toString();
-            NFCCardItem cardItem = NFCCardItem.findByCardId(cardId);
+            String cardId = edtCardId.getText().toString(); // NFCカードIDを取得する ex) "card_id_001"
+            NFCCardItem cardItem = NFCCardItem.findByCardId(cardId); // NFCカードIDから対応する情報を取得する
             if (cardItem != null) {
+
                 imgCard.setImageResource(cardItem.getResourceId());
+
+                List<Book> books = cardItem.getBooks(); // 対応する本の情報を取得する
+
+                txtBookInfo.setText(cardItem.getBooks().toString()); // debug::: 本の情報を表示する
             } else {
-                Toast.makeText(MainActivity.this, "対応する画像が見つかりませんでした", Toast.LENGTH_LONG).show();
+                // error::: 1、トーストメッセージを表示する
+                Toast.makeText(MainActivity.this, "対応するデータが見つかりませんでした", Toast.LENGTH_LONG).show();
             }
         });
     }
